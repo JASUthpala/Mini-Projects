cb369dfd282b713983dcaf5de05532e1eba09262
JASUthpala <e21416@eng.pdn.ac.lk>
2026-09-23 23:30:05 +0530

    update agent.py and snake_game.py

--- SnakeAI/snake_game.py
+++ SnakeAI/snake_game.py
@@ -59,6 +59,7 @@ class SnakeGame:
         ]
 
         self.score = 0
+        self.frame_iteration = 0
 
         self.food = None
 
@@ -76,7 +77,105 @@ class SnakeGame:
             if self.food not in self.snake:
                 break
 
-    def play_step(self):
+    def _turn_left(self, direction):
+
+        if direction == Direction.RIGHT:
+            return Direction.UP
+
+        if direction == Direction.LEFT:
+            return Direction.DOWN
+
+        if direction == Direction.UP:
+            return Direction.LEFT
+
+        if direction == Direction.DOWN:
+            return Direction.RIGHT
+
+    def _turn_right(self, direction):
+
+        if direction == Direction.RIGHT:
+            return Direction.DOWN
+
+        if direction == Direction.LEFT:
+            return Direction.UP
+
+        if direction == Direction.UP:
+            return Direction.RIGHT
+
+        if direction == Direction.DOWN:
+            return Direction.LEFT
+
+    def _get_next_head(self, direction):
+
+        x = self.head[0]
+        y = self.head[1]
+
+        if direction == Direction.RIGHT:
+            x += BLOCK_SIZE
+
+        elif direction == Direction.LEFT:
+            x -= BLOCK_SIZE
+
+        elif direction == Direction.UP:
+            y -= BLOCK_SIZE
+
+        elif direction == Direction.DOWN:
+            y += BLOCK_SIZE
+
+        return [x,y]
+
+    def _is_collision_at(self, point):
+
+        if point[0] >= WIDTH or point[0] < 0:
+            return True
+
+        if point[1] >= HEIGHT or point[1] < 0:
+            return True
+
+        if point in self.snake:
+            return True
+
+        return False
+
+    def get_state(self):
+
+        straight = self._get_next_head(self.direction)
+        right = self._get_next_head(self._turn_right(self.direction))
+        left = self._get_next_head(self._turn_left(self.direction))
+
+        state = [
+            int(self._is_collision_at(straight)),
+            int(self._is_collision_at(right)),
+            int(self._is_collision_at(left)),
+            int(self.direction == Direction.LEFT),
+            int(self.direction == Direction.RIGHT),
+            int(self.direction == Direction.UP),
+            int(self.direction == Direction.DOWN),
+            int(self.food[0] < self.head[0]),
+            int(self.food[0] > self.head[0]),
+            int(self.food[1] < self.head[1]),
+            int(self.food[1] > self.head[1])
+        ]
+
+        return state
+
+    def _move(self, action):
+
+        move_map = {
+            (1,0,0): self.direction,
+            (0,1,0): self._turn_right(self.direction),
+            (0,0,1): self._turn_left(self.direction)
+        }
+
+        direction = move_map.get(tuple(action), self.direction)
+        self.direction = direction
+
+        self.head = self._get_next_head(self.direction)
+        self.snake.insert(0, self.head)
+
+    def play_step(self, action=None):
+
+        self.frame_iteration += 1
 
         for event in pygame.event.get():
 
@@ -84,7 +183,7 @@ class SnakeGame:
                 pygame.quit()
                 quit()
 
-            if event.type == pygame.KEYDOWN:
+            if action is None and event.type == pygame.KEYDOWN:
 
                 if event.key == pygame.K_LEFT:
                     self.direction = Direction.LEFT
@@ -98,48 +197,35 @@ class SnakeGame:
                 elif event.key == pygame.K_DOWN:
                     self.direction = Direction.DOWN
 
-        x = self.head[0]
-        y = self.head[1]
+        if action is not None:
+            self._move(action)
+        else:
+            self.head = self._get_next_head(self.direction)
+            self.snake.insert(0, self.head)
 
-        if self.direction == Direction.RIGHT:
-            x += BLOCK_SIZE
-
-        elif self.direction == Direction.LEFT:
-            x -= BLOCK_SIZE
-
-        elif self.direction == Direction.UP:
-            y -= BLOCK_SIZE
-
-        elif self.direction == Direction.DOWN:
-            y += BLOCK_SIZE
-
-        self.head = [x,y]
-
-        self.snake.insert(0,self.head)
-
+        reward = 0
         game_over = False
 
         if self.is_collision():
-
             game_over = True
-
-            return game_over,self.score
+            reward = -10
+            return reward, game_over, self.score
 
         if self.head == self.food:
-
             self.score += 1
-
+            reward = 10
             self.place_food()
-
         else:
-
             self.snake.pop()
 
-        self.update_ui()
+        if self.frame_iteration > 100 * len(self.snake):
+            game_over = True
+            reward = -10
 
+        self.update_ui()
         self.clock.tick(SPEED)
 
-        return game_over,self.score
+        return reward, game_over, self.score
 
     def is_collision(self):
 
@@ -195,16 +281,26 @@ class SnakeGame:
 
 if __name__ == "__main__":
 
+    from agent import Agent
+
     game = SnakeGame()
+    agent = Agent()
 
     while True:
 
-        game_over,score = game.play_step()
+        state = game.get_state()
+        final_move = agent.get_action(state)
+        reward, game_over, score = game.play_step(final_move)
+
+        next_state = game.get_state()
+        agent.remember(state, final_move, reward, next_state, game_over)
+        agent.train_short_memory(state, final_move, reward, next_state, game_over)
 
         if game_over:
 
             print("Final Score:",score)
-
-            break
+            agent.n_games += 1
+            agent.train_long_memory()
+            game.reset()
 
     pygame.quit()
